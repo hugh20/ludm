c2e30af2b951693637856bad3e14ac73d7b72745
hugh <857504639@qq.com>
2019-05-23 00:21:17 +0800

    vip

--- app/Models/Vip.php
+++ app/Models/Vip.php
@@ -28,26 +28,4 @@ class Vip extends Model
         }
     }
 
-    public function updateAction($input)
-    {
-        try {
-            $this->fill($input)->save();
-
-            return $this->baseSucceed([], '操作成功');
-        } catch (\Exception $e) {
-            throw  $e;
-            return $this->baseFailed('内部错误');
-        }
-    }
-
-    public function destroyAction()
-    {
-        try {
-            $this->delete();
-            return $this->baseSucceed([], '删除成功');
-        } catch (\Exception $e) {
-            return $this->baseFailed('内部错误');
-        }
-
-    }
 }
